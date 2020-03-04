d9341069d9615fa34d142e6217b71bed18481c5a
abehrens13 <andrey.behrens@openaqua.de>
2020-03-04 15:13:37 +0100

    add status add some testcode as playground

--- src/main/java/de/openaqua/ipsc/JavaConfig.java
+++ src/main/java/de/openaqua/ipsc/JavaConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import de.openaqua.ipsc.beans.SerialGenerator;
+import de.openaqua.ipsc.landscape.Landscape;
 
 @Component
 public class JavaConfig {
@@ -11,6 +12,13 @@ public class JavaConfig {
 	@Bean
 	public SerialGenerator serialGenerator() {
 		return new SerialGenerator("TEST", 40000);
+	}
+
+	@Bean
+	public Landscape landscape() {
+		Landscape ls = new Landscape();
+		ls.initGermany();
+		return ls;
 	}
 
 }
